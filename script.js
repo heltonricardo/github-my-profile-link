edaae1a6d0849c8842be8ada256f6217cae408f6
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2023-06-28 18:38:52 -0300

    feat: updating script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const CONTENT_SELECTOR = "content";
-const NAVBAR_SELECTOR = "global-nav";
 const META_SELECTOR = "meta[name='user-login']";
+const MY_PROFILE_IMAGE_DIR = "assets/img/my-profile.svg";
 const MY_PROFILE_LINK_ID = "github-my-profile-link";
+const NAVBAR_SELECTOR = "AppHeader-globalBar-start";
 
 function addMyProfileLink() {
   if (document.getElementById(MY_PROFILE_LINK_ID)) return;
@@ -10,18 +11,24 @@ function addMyProfileLink() {
 
   if (userLogin) {
     const path = "/" + userLogin;
-    const navbar = document.getElementById(NAVBAR_SELECTOR);
-    const pullRequestsAnchor = navbar?.children[1];
+    const navbar = document.getElementsByClassName(NAVBAR_SELECTOR)[0];
+    const githubLogoAnchor = navbar?.children[1];
 
-    if (pullRequestsAnchor) {
+    if (githubLogoAnchor) {
+      const svgImage = document.createElement("img");
+      svgImage.width = "32";
+      svgImage.height = "32";
+      svgImage.alt = "My Profile";
+      svgImage.src = chrome.runtime.getURL(MY_PROFILE_IMAGE_DIR);
+
       const myProfileAnchor = document.createElement("a");
       myProfileAnchor.href = path;
       myProfileAnchor.id = MY_PROFILE_LINK_ID;
-      myProfileAnchor.textContent = "My profile";
       myProfileAnchor.ariaLabel = "Your Profile";
-      myProfileAnchor.className = pullRequestsAnchor.classList;
-
-      navbar.insertBefore(myProfileAnchor, pullRequestsAnchor);
+      myProfileAnchor.className = githubLogoAnchor.classList;
+      
+      myProfileAnchor.appendChild(svgImage);
+      navbar.insertBefore(myProfileAnchor, githubLogoAnchor);
     }
   }
 }
