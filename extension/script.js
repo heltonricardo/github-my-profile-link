fa32317766cadfbaf4df45ce6167cf4ed88d49cb
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2026-01-14 23:12:16 -0300

    fix: update script

--- extension/script.js
+++ extension/script.js
@@ -1,47 +1,47 @@
 const CONTENT_SELECTOR = "content";
 const META_SELECTOR = "meta[name='user-login']";
 const MY_PROFILE_LINK_ID = "github-my-profile-link";
-const NAVBAR_SELECTOR = "AppHeader-globalBar-start";
 const MY_PROFILE_IMAGE_DIR = "assets/img/my-profile.svg";
+const LOGO_SELECTOR = "a[data-component='IconButton'][href='https://github.com/']";
 
 function createSvgImage(path) {
-  const svgImage = document.createElement("img");
-  svgImage.size = "32";
-  svgImage.width = "32";
-  svgImage.height = "32";
-  svgImage.alt = "My Profile";
-  svgImage.src = `${path}.png?size=32`;
-  svgImage.classList.add("avatar", "circle");
-  svgImage.onerror = () => (svgImage.src = browser.runtime.getURL(MY_PROFILE_IMAGE_DIR));
-  return svgImage;
+    const svgImage = document.createElement("img");
+    svgImage.size = "32";
+    svgImage.width = "32";
+    svgImage.height = "32";
+    svgImage.alt = "My Profile";
+    svgImage.src = `${path}.png?size=64`;
+    svgImage.classList.add("avatar", "circle");
+    svgImage.onerror = () => (svgImage.src = browser.runtime.getURL(MY_PROFILE_IMAGE_DIR));
+    return svgImage;
 }
 
 function createProfileAnchor(path, githubLogoAnchor) {
-  const profileAnchor = document.createElement("a");
-  profileAnchor.href = path;
-  profileAnchor.id = MY_PROFILE_LINK_ID;
-  profileAnchor.ariaLabel = "Your Profile";
-  profileAnchor.className = githubLogoAnchor.classList;
-  return profileAnchor;
+    const profileAnchor = document.createElement("a");
+    profileAnchor.href = path;
+    profileAnchor.id = MY_PROFILE_LINK_ID;
+    profileAnchor.ariaLabel = "Your Profile";
+    profileAnchor.className = githubLogoAnchor.classList;
+    return profileAnchor;
 }
 
 function addMyProfileLink() {
-  if (document.getElementById(MY_PROFILE_LINK_ID)) return;
+    if (document.getElementById(MY_PROFILE_LINK_ID)) return;
 
-  const userLogin = document.querySelector(META_SELECTOR)?.getAttribute(CONTENT_SELECTOR);
+    const userLogin = document.querySelector(META_SELECTOR)?.getAttribute(CONTENT_SELECTOR);
+    if (!userLogin) return;
 
-  if (userLogin) {
-    const navbar = document.getElementsByClassName(NAVBAR_SELECTOR)[0];
-    const githubLogoAnchor = navbar?.children[1];
+    const githubLogoAnchor = document.querySelector(LOGO_SELECTOR);
+    if (!githubLogoAnchor) return;
 
-    if (githubLogoAnchor) {
-      const path = "/" + userLogin;
-      const svgImage = createSvgImage(path);
-      const myProfileAnchor = createProfileAnchor(path, githubLogoAnchor);
-      myProfileAnchor.appendChild(svgImage);
-      navbar.insertBefore(myProfileAnchor, githubLogoAnchor);
-    }
-  }
+    const container = githubLogoAnchor.parentNode;
+    if (!container) return;
+
+    const path = "/" + userLogin;
+    const svgImage = createSvgImage(path);
+    const myProfileAnchor = createProfileAnchor(path, githubLogoAnchor);
+    myProfileAnchor.appendChild(svgImage);
+    container.insertBefore(myProfileAnchor, githubLogoAnchor);
 }
 
 new MutationObserver(addMyProfileLink).observe(document, { childList: true, subtree: true });
